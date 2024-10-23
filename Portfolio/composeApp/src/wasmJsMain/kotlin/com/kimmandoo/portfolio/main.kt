3a7aeb22bdab4fdf788484d7c619d7847773ed93
Mingyu Kim <mingyu5675@gmail.com>
2024-10-23 17:34:40 +0900

    feat: favicon

--- Portfolio/composeApp/src/wasmJsMain/kotlin/com/kimmandoo/portfolio/main.kt
+++ Portfolio/composeApp/src/wasmJsMain/kotlin/com/kimmandoo/portfolio/main.kt
@@ -6,6 +6,13 @@ import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    val link = document.querySelector("link[rel~='icon']")
+    	?: document.createElement("link").apply {
+            setAttribute("rel", "icon")
+        	document.head?.appendChild(this);
+        }
+    link.setAttribute("href", "https://kimmandooo.tistory.com/favicon.ico");
+
     ComposeViewport(document.body!!) {
         App()
     }
