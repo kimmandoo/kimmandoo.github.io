61df61dea3e3bf666e2c6b10279f612ec1bd3a9d
kimmandoo <mingyu5675@gmail.com>
2025-02-23 23:51:05 +0900

    init

--- Portfolio/composeApp/src/commonMain/kotlin/com/kimmandoo/portfolio/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.kimmandoo.portfolio
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
---
+++ blog/composeApp/src/wasmJsMain/kotlin/com/kimmandoo/blog/Platform.kt
@@ -0,0 +1,7 @@
+package com.kimmandoo.blog
+
+class WasmPlatform {
+    val name: String = "Web with Kotlin/Wasm"
+}
+
+fun getPlatform() = WasmPlatform()
